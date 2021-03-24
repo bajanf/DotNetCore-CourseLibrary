e914dc1de2977a08c720cf18503d100c7d382d3c
Bajan <f.bajan@accenture.com>
2021-03-24 22:20:42 +0200

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")] //or [Route("api/[controller]")]
+    public class AuthorsController: ControllerBase
+    {
+        private ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()] //[HttpGet("api/authors")] no nee because we set [Route("api/authors")] up
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+
+            return new JsonResult(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return new JsonResult(authorFromRepo);
+        }
+
+    }
+}
