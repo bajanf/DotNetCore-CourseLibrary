b1bce91f88ffc2520269fe0d7b109182fdeb3b79
f.bajan <flavia.bajan@gmail.com>
2021-03-29 21:39:02 +0300

    Validiting data & reporting validation error

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,14 +1,30 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(
+        ErrorMessage ="Title must be different from description")]
+    public class CourseForCreationDto//: IValidatableObject
     {
+        [Required (ErrorMessage ="You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The Title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
 
+        [MaxLength(100, ErrorMessage = "The Description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title==Description)
+        //    {
+        //        yield return new ValidationResult("The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
